9c07510880636c7a4130c3e3967e612c3d8a33ec
Ubaid Ahmad <bhatubaid341@gmail.com>
2026-08-01 15:58:31 +0530

    Remove test script

--- test.ts
+++
@@ -1,8 +0,0 @@
-import prisma from './src/lib/prisma';
-async function main() {
-  const p = await prisma.property.findFirst({
-    where: { name: { contains: 'Peer Homestay' } }
-  });
-  console.log(JSON.stringify(p?.images));
-}
-main();
