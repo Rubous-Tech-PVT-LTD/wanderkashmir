363e78a0dbefa2a80f6c3bddf51e42f07f4230e9
Ubaid Ahmad <bhatubaid341@gmail.com>
2026-08-01 16:26:30 +0530

    Add LinkedIn icon and update social media links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,6 +25,12 @@ const XIcon = ({ className }: { className?: string }) => (
   </svg>
 );
 
+const LinkedinIcon = ({ className }: { className?: string }) => (
+  <svg className={className} fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+    <path fillRule="evenodd" d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" clipRule="evenodd" />
+  </svg>
+);
+
 const footerLinks = {
   Explore: [
     { label: "Hotels & Resorts", href: "/stays?type=hotel" },
@@ -131,10 +137,11 @@ export default function Footer() {
             {/* Social */}
             <div className="flex gap-3">
               {[
-                { Icon: InstagramIcon, href: "#", label: "Instagram" },
-                { Icon: FacebookIcon, href: "#", label: "Facebook" },
-                { Icon: YoutubeIcon, href: "#", label: "YouTube" },
-                { Icon: XIcon, href: "#", label: "Twitter" },
+                { Icon: InstagramIcon, href: "https://instagram.com/wanderkashmir", label: "Instagram" },
+                { Icon: FacebookIcon, href: "https://facebook.com/wanderkashmir", label: "Facebook" },
+                { Icon: YoutubeIcon, href: "https://youtube.com/@wanderkashmir", label: "YouTube" },
+                { Icon: XIcon, href: "https://x.com/wanderkashmir", label: "Twitter" },
+                { Icon: LinkedinIcon, href: "https://linkedin.com/company/wanderkashmir", label: "LinkedIn" },
               ].map(({ Icon, href, label }) => (
                 <a
                   key={label}
